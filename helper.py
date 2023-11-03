0b7751701efd36f4d9ecbb6419e85097595acc4b
Smrithy Sajan <smudavangattilsajan@albany.edu>
2023-11-03 13:13:13 -0400

    updated functions

--- helper.py
+++ helper.py
@@ -1,9 +1,17 @@
 import todofunctions
 
+def display_options():
+    print("Choose an option:")
+    print("1. Add a new task")
+    print("2. Mark task as completed")
+    print("3. Remove the task")
+    print("4. Edit pre-existing task")
+    print("5. Add priority")
+
 def main():
     list_obj = todofunctions.todoList()
-    list_obj.add_task("Eat dinner")
-    list_obj.add_task("Finish dinner")
+    #list_obj.add_task("Eat dinner")
+    list_obj.add_task("Finish dinner",3)
     list_obj.display_list()
 
 main()
--- todofunctions.py
+++ todofunctions.py
@@ -3,20 +3,32 @@
 
 class todoList:
     def __init__(self):
-        #tasks is a list of tasks the user wants to add
-        self.tasks = [] 
+        #dictionary to add task
+        self.tasks = {} 
     
     #add task
-    def add_task(self,task):
-        self.tasks.append(task)
-        print("Task added")
+    # def add_task(self,task):
+    #     numtasks = len(self.tasks)
+    #     self.tasks[numtasks+1] = {'Task':task, 'Priority': 0, 'Completed':False }
+    #     print("Task added in 1")
+    
+    #To add task, sets priority for the task, and defaults the completed to False
+    def add_task(self,task,priority):
+        numtasks = len(self.tasks)
+        self.tasks[numtasks+1] = {'Task':task, 'Priority': priority, 'Completed':False }
+        print("Task added sucessfully")
 
     #displays the current list
     def display_list(self):
-        print("To-do list:")
-        for i in range(len(self.tasks)):
-            print(str(i) + ":" + self.tasks[i])
+        print("To-do list")
+        print("----------")
+        for k,v in self.tasks.items():
+            print("Task ID:", k)
 
+            for key in v:
+                print(key + ':', v[key])
+
+            print("\n")
     #remove task
     #display task
 
